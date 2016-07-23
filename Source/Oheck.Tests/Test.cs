faef94dd172f906cc34299b51f9832cc2f41e8fc
Dan Watkins <dwatkins@valpineware.com>
2016-07-23 14:25:17 -0500

    Added core Oheck library with NUnit 3 test project.

---
+++ Source/Oheck.Tests/Test.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Oheck.Tests
+{
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        public void Test1()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
